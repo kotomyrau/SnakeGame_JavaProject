9f1ed8f468d8fe3c3230f132ad9445d2921685d7
Kotomyra Uchida <kotomyrau@gmail.com>
2021-02-09 17:04:44 +0000

    panel constructor, methods and inside classes added

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,11 +1,47 @@
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.Random;
 
 public class GamePanel extends JPanel implements ActionListener {
-
+// CONSTRUCTOR FOR GAME PANEL
     GamePanel(){
 
+    }
+//    METHODS
+    public void startGame() {
+        
+    }
+
+    public void paintComponent(Graphics g) {
+
+    }
+
+    public void draw(Graphics g) {
+
+    }
+
+    public void move() {
+
+    }
+
+    public void checkApple() {
+
+    }
+
+    public void checkCollisions() {
+
+    }
+
+    public void gameOver(Graphics g) {
+
+    }
+//    INNER CLASS
+    public class MyKeyAdapter extends KeyAdapter{
+        @Override
+        public void keyPressed(KeyEvent e) {
+
+        }
     }
     @Override
     public void actionPerformed(ActionEvent e) {
